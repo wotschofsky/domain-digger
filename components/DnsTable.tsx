f691e9ca92644ba6a101d1fa813f065cbd10917e
Felix Wotschofsky <hello@felisk.io>
2022-03-10 22:27:32 +0100

    Allow horizontal scrolling of results ↔️

--- components/DnsTable.tsx
+++ components/DnsTable.tsx
@@ -1,5 +1,6 @@
 import { Fragment } from 'react';
-import { Heading, Table, Tbody, Th, Thead, Tr } from '@chakra-ui/react';
+import { chakra, Heading, Table, Tbody, Th, Thead, Tr } from '@chakra-ui/react';
+import { css } from '@emotion/react';
 
 import { ResolvedRecords } from '@/utils/DnsLookup';
 import RecordRow from '@/components/RecordRow';
@@ -22,20 +23,26 @@ const DnsTable = ({ records }: DnsTableProps) => (
           <Heading as="h2" fontSize={{ base: 'xl', sm: '2xl' }} mb={4} mt={8}>
             {recordType}
           </Heading>
-          <Table key={recordType}>
-            <Thead>
-              <Tr>
-                <Th pl={0}>Name</Th>
-                <Th>TTL</Th>
-                <Th pr={0}>Value</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {value.map((v) => (
-                <RecordRow key={v.type + v.data} record={v} />
-              ))}
-            </Tbody>
-          </Table>
+          <chakra.div
+            css={css`
+              overflow-x: auto;
+            `}
+          >
+            <Table key={recordType}>
+              <Thead>
+                <Tr>
+                  <Th pl={0}>Name</Th>
+                  <Th>TTL</Th>
+                  <Th pr={0}>Value</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {value.map((v) => (
+                  <RecordRow key={v.type + v.data} record={v} />
+                ))}
+              </Tbody>
+            </Table>
+          </chakra.div>
         </Fragment>
       );
     })}
