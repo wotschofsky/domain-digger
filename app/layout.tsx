75eac0e7484b413424590b9c26b2b3c249e222b0
Felix Wotschofsky <hello@felisk.io>
2023-05-21 03:08:59 +0200

    Remove Chakra UI ♻️

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import type { ReactNode } from 'react';
 import { SWRConfig } from 'swr';
 
@@ -17,19 +16,16 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <html lang="en">
       <body>
-        <ColorModeScript initialColorMode="system" />
         <SWRConfig
           value={{ fetcher: (url) => fetch(url).then((res) => res.json()) }}
         >
-          <ChakraProvider>
-            <div className="flex min-h-screen flex-col items-center justify-center">
-              <Header />
+          <div className="flex min-h-screen flex-col items-center justify-center">
+            <Header />
 
-              <div className="w-full flex-1">{children}</div>
+            <div className="w-full flex-1">{children}</div>
 
-              <Footer />
-            </div>
-          </ChakraProvider>
+            <Footer />
+          </div>
         </SWRConfig>
       </body>
     </html>
