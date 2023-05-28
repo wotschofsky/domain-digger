7a3eb3bc11387739ebf3fc492aa2755127f82e38
Felix Wotschofsky <hello@felisk.io>
2023-05-28 15:09:53 +0200

    Add sr-only text in footer 🗣️

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,9 +8,13 @@ const Footer = () => (
   <footer className="w-full p-4 md:px-8">
     <div className="flex flex-col items-center justify-between border-t border-t-gray-200 pt-4 sm:flex-row">
       <p className="text-sm">
-        Created with{' '}
-        <FaHeart className="inline text-red-500" fontSize="1.25rem" /> by{' '}
-        <a href="https://felisk.io">Felix Wotschofsky</a>
+        Created with
+        <>
+          {' '}
+          <FaHeart className="inline text-red-500" fontSize="1.25rem" />
+          <span className="sr-only">love</span>{' '}
+        </>
+        by <a href="https://felisk.io">Felix Wotschofsky</a>
       </p>
 
       <Button variant="ghost" asChild>
