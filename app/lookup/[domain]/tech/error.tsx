8b4d226c8bbc93ca6194e5f3bd1537107e0762d1
Mathis <mathis.fullriede@gmail.com>
2023-06-24 15:42:01 +0200

    Revert "init"
    This reverts commit fabcd6721d381c0cafed8aa5737f17133d3cdb0f.

--- app/lookup/[domain]/tech/error.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-const TechError = () => (
-  <div className="flex items-center justify-center">
-    <p className="my-8">An error occurred!</p>
-  </div>
-);
-
-export default TechError;
--- app/lookup/[domain]/tech/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Spinner } from '@/components/ui/spinner';
-
-const TechLoading = () => (
-  <div className="flex items-center justify-center">
-    <Spinner className="my-8" />
-  </div>
-);
-
-export default TechLoading;
