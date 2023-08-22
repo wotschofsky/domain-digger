7706ab518e1e8cbc177073abb9bdf02d3cf2d1c2
Felix Wotschofsky <felix@wotschofsky.com>
2023-08-22 12:59:28 +0200

    Improve tooltip integration 👌

--- components/DomainLink.tsx
+++ components/DomainLink.tsx
@@ -17,7 +17,7 @@ const DomainLink: FC<DomainLinkProps> = ({ domain }) => (
   <>
     <TooltipProvider>
       <Tooltip>
-        <TooltipTrigger>
+        <TooltipTrigger asChild>
           <Link
             className="select-all underline decoration-dotted underline-offset-4"
             href={`/lookup/${domain}`}
@@ -33,7 +33,7 @@ const DomainLink: FC<DomainLinkProps> = ({ domain }) => (
     {!domain.startsWith('*.') && (
       <TooltipProvider>
         <Tooltip>
-          <TooltipTrigger>
+          <TooltipTrigger asChild>
             <a href={`https://${domain}`} target="_blank" rel="noreferrer">
               <ExternalLinkIcon className="mx-1 inline-block h-3 w-3 -translate-y-0.5" />
             </a>
