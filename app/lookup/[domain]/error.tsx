4608ef5eeb77871c7d68b9b66201a1c1874d7d15
Felix Wotschofsky <felix@wotschofsky.com>
2023-10-02 14:35:59 +0200

    Improve error pages 👌

--- app/lookup/[domain]/error.tsx
+++ app/lookup/[domain]/error.tsx
@@ -1,26 +1,16 @@
 'use client';
 
-import { Loader2 } from 'lucide-react';
-import { type FC, useCallback, useEffect, useState } from 'react';
+import { type FC, useEffect } from 'react';
 
-import { Button } from '@/components/ui/button';
-
-type LookupDomainErrorProps = {
+type DomainErrorProps = {
   error: Error & { digest?: string };
   reset: () => void;
 };
 
-const LookupDomainError: FC<LookupDomainErrorProps> = ({ error, reset }) => {
-  const [isLoading, setLoading] = useState(false);
-
+const DomainError: FC<DomainErrorProps> = ({ error }) => {
   useEffect(() => {
     console.error(error);
   }, [error]);
-
-  const handleReset = useCallback(() => {
-    setLoading(true);
-    reset();
-  }, [reset]);
 
   return (
     <div className="mt-12 flex flex-col items-center gap-2">
@@ -28,17 +18,8 @@ const LookupDomainError: FC<LookupDomainErrorProps> = ({ error, reset }) => {
       <p className="mt-2 text-center text-sm text-muted-foreground">
         Digest: {error.digest}
       </p>
-      <Button
-        className="mt-4"
-        onClick={handleReset}
-        disabled={isLoading}
-        variant="outline"
-      >
-        {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
-        Try again
-      </Button>
     </div>
   );
 };
 
-export default LookupDomainError;
+export default DomainError;
