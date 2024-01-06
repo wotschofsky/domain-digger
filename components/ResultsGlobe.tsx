da0f044f7d8458e9bdf6b0398d1ab92a3a94324b
Felix Wotschofsky <felix@wotschofsky.com>
2024-01-06 15:27:43 +0100

    support dark mode

--- components/ResultsGlobe.tsx
+++ components/ResultsGlobe.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useTheme } from 'next-themes';
 import dynamic from 'next/dynamic';
 import type { FC } from 'react';
 
@@ -18,24 +19,31 @@ type ResultsGlobeProps = {
   }[];
 };
 
-const ResultsGlobe: FC<ResultsGlobeProps> = ({ markers }) => (
-  <Globe
-    // Map from https://github.com/vasturiano/three-globe/blob/7717c8e54c98dcb3e92df5a95923f51387a496a1/example/img/earth-day.jpg
-    globeImageUrl="/assets/earth-day.jpg"
-    backgroundColor="rgba(0,0,0,0)"
-    htmlElementsData={markers}
-    // @ts-expect-error
-    htmlElement={(d: ResultsGlobeProps['markers'][number]) => {
-      const el = document.createElement('div');
-      el.innerHTML = `<div class="relative bg-white p-2 rounded-lg shadow-md">
-        <h3 class="text-sm font-bold mb-2">${d.name}</h3>
-        ${d.results.A.map(
-          (ip) => `<p class="text-xs text-gray-500">${ip}</p>`
-        ).join('')}
-      </div>`;
-      return el;
-    }}
-  />
-);
+const ResultsGlobe: FC<ResultsGlobeProps> = ({ markers }) => {
+  const { theme } = useTheme();
+
+  return (
+    <Globe
+      // Map from https://github.com/vasturiano/three-globe
+      globeImageUrl={
+        theme === 'dark' ? '/assets/earth-night.jpg' : '/assets/earth-day.jpg'
+      }
+      backgroundColor="rgba(0,0,0,0)"
+      onZoom={() => false}
+      htmlElementsData={markers}
+      // @ts-expect-error
+      htmlElement={(d: ResultsGlobeProps['markers'][number]) => {
+        const el = document.createElement('div');
+        el.innerHTML = `<div class="relative bg-background p-2 rounded-lg shadow-md">
+          <h3 class="text-sm font-bold mb-2">${d.name}</h3>
+          ${d.results.A.map(
+            (ip) => `<p class="text-xs text-muted-foreground">${ip}</p>`
+          ).join('')}
+        </div>`;
+        return el;
+      }}
+    />
+  );
+};
 
 export default ResultsGlobe;
