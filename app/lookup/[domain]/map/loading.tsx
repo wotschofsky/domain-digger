14058cbc14d0c57f537acfc752da7e04ce7d8922
Felix Wotschofsky <felix@wotschofsky.com>
2024-01-07 23:06:36 +0100

    more finishing touches

--- app/lookup/[domain]/map/loading.tsx
+++ app/lookup/[domain]/map/loading.tsx
@@ -1,9 +1,9 @@
 import { Skeleton } from '@/components/ui/skeleton';
 
-const WhoisLoading = () => (
+const MapLoading = () => (
   <div className="mt-12 flex justify-center">
-    <Skeleton className="my-36 aspect-square w-4/5 rounded-full" />
+    <Skeleton className="my-36 aspect-square w-3/5 rounded-full" />
   </div>
 );
 
-export default WhoisLoading;
+export default MapLoading;
