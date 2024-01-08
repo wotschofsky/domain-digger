15e81103d17e3999ac29b6d0d49865fae4c7f614
Felix Wotschofsky <felix@wotschofsky.com>
2024-01-08 14:00:03 +0100

    Use `hyperscript` for globe markers ✨

--- components/ResultsGlobe.tsx
+++ components/ResultsGlobe.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import h from 'hyperscript';
 import { useTheme } from 'next-themes';
 import dynamic from 'next/dynamic';
 import { type FC, useEffect, useRef, useState } from 'react';
@@ -59,70 +60,68 @@ const ResultsGlobe: FC<ResultsGlobeProps> = ({ markers }) => {
         htmlElementsData={markers}
         // @ts-expect-error
         htmlElement={(d: ResultsGlobeProps['markers'][number]) => {
-          const el = document.createElement('div');
-          el.classList.add('marker-wrapper');
-          el.innerHTML = `<div class="flex flex-col gap-2 rounded-lg bg-background p-2 shadow-md">
-            <h3 class="text-sm font-bold">${d.name}</h3>
-            <div>
-              ${d.results.A.slice(0, 4)
-                .map(
-                  (value) =>
-                    `<p class="text-xs text-muted-foreground">${value}</p>`
-                )
-                .join('')}
-              ${
-                d.results.A.length > 4
-                  ? `<p class="text-xs text-muted-foreground italic">and ${
-                      d.results.A.length - 4
-                    } more</p>`
-                  : ''
-              }
-            </div>
+          return h(
+            'div.marker-wrapper',
 
-            <div>
-              ${d.results.AAAA.slice(0, 4)
-                .map(
-                  (value) =>
-                    `<p class="text-xs text-muted-foreground">${value}</p>`
-                )
-                .join('')}
-              ${
-                d.results.AAAA.length > 4
-                  ? `<p class="text-xs text-muted-foreground italic">and ${
-                      d.results.AAAA.length - 4
-                    } more</p>`
-                  : ''
-              }
-            </div>
+            h(
+              'div.flex.flex-col.gap-2.rounded-lg.bg-background.p-2.shadow-md',
 
+              h('h3.text-sm.font-bold', d.name),
+              d.results.A.length
+                ? h(
+                    'div',
+                    d.results.A.slice(0, 4).map((value) =>
+                      h('p.text-xs.text-muted-foreground', value)
+                    ),
+                    d.results.A.length > 4
+                      ? h(
+                          'p.text-xs.text-muted-foreground.italic',
+                          `and ${d.results.A.length - 4} more`
+                        )
+                      : undefined
+                  )
+                : undefined,
 
-            <div>
-              ${d.results.CNAME.slice(0, 4)
-                .map(
-                  (value) =>
-                    `<p class="text-xs text-muted-foreground">${value}</p>`
-                )
-                .join('')}
-              ${
-                d.results.CNAME.length > 4
-                  ? `<p class="text-xs text-muted-foreground italic">and ${
-                      d.results.CNAME.length - 4
-                    } more</p>`
-                  : ''
-              }
-            </div>
+              d.results.AAAA.length
+                ? h(
+                    'div',
+                    d.results.AAAA.slice(0, 4).map((value) =>
+                      h('p.text-xs.text-muted-foreground', value)
+                    ),
+                    d.results.AAAA.length > 4
+                      ? h(
+                          'p.text-xs.text-muted-foreground.italic',
+                          `and ${d.results.AAAA.length - 4} more`
+                        )
+                      : undefined
+                  )
+                : undefined,
 
-            ${
+              d.results.CNAME.length
+                ? h(
+                    'div',
+                    d.results.CNAME.slice(0, 4).map((value) =>
+                      h('p.text-xs.text-muted-foreground', value)
+                    ),
+                    d.results.CNAME.length > 4
+                      ? h(
+                          'p.text-xs.text-muted-foreground.italic',
+                          `and ${d.results.CNAME.length - 4} more`
+                        )
+                      : undefined
+                  )
+                : undefined,
+
               d.results.A.length === 0 &&
-              d.results.AAAA.length === 0 &&
-              d.results.CNAME.length === 0
-                ? `
-                  <p class="text-xs text-muted-foreground italic">No records found!</p>
-                `
-                : ''
-            }
-          </div>`;
-          return el;
+                d.results.AAAA.length === 0 &&
+                d.results.CNAME.length === 0
+                ? h(
+                    'p.text-xs.text-muted-foreground.italic',
+                    'No records found!'
+                  )
+                : undefined
+            )
+          );
         }}
       />
     </div>
