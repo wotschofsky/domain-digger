957e02fc1eea3be4a25b54acf0a218c0597ef3b0
Felix Wotschofsky <felix@wotschofsky.com>
2024-01-13 12:01:47 +0100

    Refactor ResultsTabs ✨

--- components/ResultsTabs.tsx
+++ components/ResultsTabs.tsx
@@ -12,6 +12,36 @@ const isAppleDevice = () => {
   return /Mac|iPad|iPhone|iPod/.test(userAgent);
 };
 
+const SingleTab: FC<{
+  label: string;
+  href: string;
+  selected: boolean;
+  shortcutNumber: number;
+}> = ({ label, href, selected, shortcutNumber }) => (
+  <li className="mr-2">
+    <Link
+      href={href}
+      className={
+        selected
+          ? 'relative inline-block rounded-t-lg border-b-2 border-primary p-4 text-primary'
+          : 'relative inline-block rounded-t-lg border-b-2 border-transparent p-4 hover:border-gray-300 hover:text-gray-600 dark:hover:text-gray-300'
+      }
+    >
+      {label}
+      <span className="pointer-events-none absolute bottom-0 left-1/2 block w-full -translate-x-1/2 translate-y-4 text-xs text-muted-foreground opacity-0 transition-all group-hover:translate-y-6 group-hover:opacity-100">
+        {isAppleDevice() ? (
+          <>
+            <OptionIcon className="inline-block h-3 w-3" strokeWidth={3} />
+            {` + ${shortcutNumber}`}
+          </>
+        ) : (
+          `alt+${shortcutNumber}`
+        )}
+      </span>
+    </Link>
+  </li>
+);
+
 type ResultsTabsProps = {
   domain: string;
 };
@@ -28,110 +58,30 @@ const ResultsTabs: FC<ResultsTabsProps> = ({ domain }) => {
   return (
     <div className="group mb-6 mt-6 border-b border-gray-200 text-center text-sm font-medium text-gray-500 dark:border-gray-700 dark:text-gray-400">
       <ul className="-mb-px flex flex-wrap">
-        <li className="mr-2">
-          <Link
-            href={`/lookup/${domain}`}
-            className={
-              selectedSegment === null
-                ? 'relative inline-block rounded-t-lg border-b-2 border-primary p-4 text-primary'
-                : 'relative inline-block rounded-t-lg border-b-2 border-transparent p-4 hover:border-gray-300 hover:text-gray-600 dark:hover:text-gray-300'
-            }
-          >
-            DNS
-            <span className="pointer-events-none absolute bottom-0 left-1/2 block w-full -translate-x-1/2 translate-y-4 text-xs text-muted-foreground opacity-0 transition-all group-hover:translate-y-6 group-hover:opacity-100">
-              {isAppleDevice() ? (
-                <>
-                  <OptionIcon
-                    className="inline-block h-3 w-3"
-                    strokeWidth={3}
-                  />
-                  {' + 1'}
-                </>
-              ) : (
-                'alt+1'
-              )}
-            </span>
-          </Link>
-        </li>
-        <li className="mr-2">
-          <Link
-            href={`/lookup/${domain}/map`}
-            className={
-              selectedSegment === 'map'
-                ? 'relative inline-block rounded-t-lg border-b-2 border-primary p-4 text-primary'
-                : 'relative inline-block rounded-t-lg border-b-2 border-transparent p-4 hover:border-gray-300 hover:text-gray-600 dark:hover:text-gray-300'
-            }
-            aria-current="page"
-          >
-            DNS Map
-            <span className="pointer-events-none absolute bottom-0 left-1/2 block w-full -translate-x-1/2 translate-y-4 text-xs text-muted-foreground opacity-0 transition-all group-hover:translate-y-6 group-hover:opacity-100">
-              {isAppleDevice() ? (
-                <>
-                  <OptionIcon
-                    className="inline-block h-3 w-3"
-                    strokeWidth={3}
-                  />
-                  {' + 2'}
-                </>
-              ) : (
-                'alt+2'
-              )}
-            </span>
-          </Link>
-        </li>
-        <li className="mr-2">
-          <Link
-            href={`/lookup/${domain}/whois`}
-            className={
-              selectedSegment === 'whois'
-                ? 'relative inline-block rounded-t-lg border-b-2 border-primary p-4 text-primary'
-                : 'relative inline-block rounded-t-lg border-b-2 border-transparent p-4 hover:border-gray-300 hover:text-gray-600 dark:hover:text-gray-300'
-            }
-            aria-current="page"
-          >
-            Whois
-            <span className="pointer-events-none absolute bottom-0 left-1/2 block w-full -translate-x-1/2 translate-y-4 text-xs text-muted-foreground opacity-0 transition-all group-hover:translate-y-6 group-hover:opacity-100">
-              {isAppleDevice() ? (
-                <>
-                  <OptionIcon
-                    className="inline-block h-3 w-3"
-                    strokeWidth={3}
-                  />
-                  {' + 3'}
-                </>
-              ) : (
-                'alt+3'
-              )}
-            </span>
-          </Link>
-        </li>
-
-        <li className="mr-2">
-          <Link
-            href={`/lookup/${domain}/certs`}
-            className={
-              selectedSegment === 'certs'
-                ? 'relative inline-block rounded-t-lg border-b-2 border-primary p-4 text-primary'
-                : 'relative inline-block rounded-t-lg border-b-2 border-transparent p-4 hover:border-gray-300 hover:text-gray-600 dark:hover:text-gray-300'
-            }
-            aria-current="page"
-          >
-            Certs
-            <span className="pointer-events-none absolute bottom-0 left-1/2 block w-full -translate-x-1/2 translate-y-4 text-xs text-muted-foreground opacity-0 transition-all group-hover:translate-y-6 group-hover:opacity-100">
-              {isAppleDevice() ? (
-                <>
-                  <OptionIcon
-                    className="inline-block h-3 w-3"
-                    strokeWidth={3}
-                  />
-                  {' + 4'}
-                </>
-              ) : (
-                'alt+4'
-              )}
-            </span>
-          </Link>
-        </li>
+        <SingleTab
+          label="DNS"
+          href={`/lookup/${domain}`}
+          selected={selectedSegment === null}
+          shortcutNumber={1}
+        />
+        <SingleTab
+          label="DNS Map"
+          href={`/lookup/${domain}/map`}
+          selected={selectedSegment === 'map'}
+          shortcutNumber={2}
+        />
+        <SingleTab
+          label="Whois"
+          href={`/lookup/${domain}/whois`}
+          selected={selectedSegment === 'whois'}
+          shortcutNumber={3}
+        />
+        <SingleTab
+          label="Certs"
+          href={`/lookup/${domain}/certs`}
+          selected={selectedSegment === 'certs'}
+          shortcutNumber={4}
+        />
       </ul>
     </div>
   );
