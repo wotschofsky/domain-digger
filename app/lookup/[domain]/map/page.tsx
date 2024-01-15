a15c29c0245cc9a7d7101a07d4e2ad7455c8d4fa
Felix Wotschofsky <felix@wotschofsky.com>
2024-01-16 00:56:40 +0100

    Filter requests to map page from bots 🛡️

--- app/lookup/[domain]/map/page.tsx
+++ app/lookup/[domain]/map/page.tsx
@@ -1,4 +1,9 @@
+import { isbot } from 'isbot';
+import { ShieldAlertIcon } from 'lucide-react';
+import { headers } from 'next/headers';
 import type { FC } from 'react';
+
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 
 import ResultsGlobe from '@/components/ResultsGlobe';
 import { REGIONS } from '@/lib/data';
@@ -15,6 +20,25 @@ type MapResultsPageProps = {
 const MapResultsPage: FC<MapResultsPageProps> = async ({
   params: { domain },
 }) => {
+  const userAgent = headers().get('user-agent');
+  const shouldBlockRequest = !userAgent || isbot(userAgent);
+
+  if (shouldBlockRequest) {
+    console.log('Bot detected, blocking request, UA:', userAgent);
+    return (
+      <Alert className="mx-auto mt-24 w-max">
+        <ShieldAlertIcon className="h-4 w-4" />
+        <AlertTitle>Bot or crawler detected!</AlertTitle>
+        <AlertDescription>
+          To protect our infrastructure, this page is not available for bots or
+          crawlers.
+          <br />
+          But don&apos;t be sad, there&apos;s to crawl here anyway.
+        </AlertDescription>
+      </Alert>
+    );
+  }
+
   const markers = await Promise.all(
     Object.entries(REGIONS).map(async ([code, data]) => {
       const resolver = new InternalDoHResolver(code, 'cloudflare');
