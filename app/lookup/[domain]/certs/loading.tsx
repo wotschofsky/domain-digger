90e1ecae4de5e4bfbed4f0381c77a15dfacdc0d0
Felix Wotschofsky <felix@wotschofsky.com>
2024-01-18 12:52:17 +0100

    Improve loading screen for certs and subdomains ⏳

--- app/lookup/[domain]/certs/loading.tsx
+++ app/lookup/[domain]/certs/loading.tsx
@@ -1,9 +1,50 @@
-import { Spinner } from '@/components/ui/spinner';
+import { Skeleton } from '@/components/ui/skeleton';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
 
 const CertsLoading = () => (
-  <div className="flex items-center justify-center">
-    <Spinner className="my-8" />
-  </div>
+  <Table>
+    <TableHeader>
+      <TableRow className="hover:bg-transparent">
+        <TableHead className="pl-0">Logged At</TableHead>
+        <TableHead>Not Before</TableHead>
+        <TableHead>Not After</TableHead>
+        <TableHead>Common Name</TableHead>
+        <TableHead>Matching Identities</TableHead>
+        <TableHead className="pr-0">Issuer Name</TableHead>
+      </TableRow>
+    </TableHeader>
+    <TableBody>
+      {Array.from({ length: 8 }).map((_, i) => (
+        <TableRow key={i} className="hover:bg-transparent">
+          <TableCell className="pl-0">
+            <Skeleton className="h-5 w-44 rounded-sm" />
+          </TableCell>
+          <TableCell>
+            <Skeleton className="h-5 w-44 rounded-sm" />
+          </TableCell>
+          <TableCell>
+            <Skeleton className="h-5 w-44 rounded-sm" />
+          </TableCell>
+          <TableCell>
+            <Skeleton className="h-5 w-48 rounded-sm" />
+          </TableCell>
+          <TableCell>
+            <Skeleton className="h-5 w-48 rounded-sm" />
+          </TableCell>
+          <TableCell className="pr-0">
+            <Skeleton className="h-5 w-64 rounded-sm" />
+          </TableCell>
+        </TableRow>
+      ))}
+    </TableBody>
+  </Table>
 );
 
 export default CertsLoading;
--- app/lookup/[domain]/subdomains/loading.tsx
+++ app/lookup/[domain]/subdomains/loading.tsx
@@ -1,9 +1,38 @@
-import { Spinner } from '@/components/ui/spinner';
+import { Skeleton } from '@/components/ui/skeleton';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
 
 const SubdomainsLoading = () => (
-  <div className="flex items-center justify-center">
-    <Spinner className="my-8" />
-  </div>
+  <Table>
+    <TableHeader>
+      <TableRow className="hover:bg-transparent">
+        <TableHead className="pl-0">Domain Name</TableHead>
+        <TableHead>First seen</TableHead>
+        <TableHead className="pr-0">Still exists</TableHead>
+      </TableRow>
+    </TableHeader>
+    <TableBody>
+      {Array.from({ length: 8 }).map((_, i) => (
+        <TableRow key={i} className="hover:bg-transparent">
+          <TableCell className="pl-0">
+            <Skeleton className="h-5 w-64 rounded-sm" />
+          </TableCell>
+          <TableCell>
+            <Skeleton className="h-5 w-80 rounded-sm" />
+          </TableCell>
+          <TableCell className="pr-0">
+            <Skeleton className="h-5 w-5 rounded-sm" />
+          </TableCell>
+        </TableRow>
+      ))}
+    </TableBody>
+  </Table>
 );
 
 export default SubdomainsLoading;
