5ae035e319facd2914644a61fd76ff9ea1f4a361
Felix Wotschofsky <felix@wotschofsky.com>
2024-03-17 16:53:55 +0100

    Add Product Hunt badge 🏅

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -66,7 +66,22 @@ const Home = () => {
           <h2 className="text-center font-semibold sm:text-lg">
             Trusted by experts at
           </h2>
+
           <TrustedByLogos />
+
+          <a
+            href="https://www.producthunt.com/posts/domain-digger?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-domain&#0045;digger"
+            target="_blank"
+            className="mt-4"
+          >
+            <img
+              src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=434616&theme=neutral"
+              alt="Domain&#0032;Digger - Full&#0032;open&#0045;source&#0032;toolkit&#0032;for&#0032;next&#0045;level&#0032;domain&#0032;analysis | Product Hunt"
+              style={{ width: '250px', height: '54px' }}
+              width="250"
+              height="54"
+            />
+          </a>
         </div>
       </section>
 
