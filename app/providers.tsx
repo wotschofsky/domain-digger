ecab2c32f520fdc036d6ca3e45b028e894386617
Felix Wotschofsky <felix@wotschofsky.com>
2024-03-28 16:43:21 +0100

    Make Plausible Analytics configurable through env 🔧

--- app/providers.tsx
+++ app/providers.tsx
@@ -5,6 +5,28 @@ import { ThemeProvider } from 'next-themes';
 import { type FC, type ReactNode } from 'react';
 import { SWRConfig } from 'swr';
 
+type CustomizedPlausibleProviderProps = {
+  children: ReactNode;
+};
+
+const CustomizedPlausibleProvider: FC<CustomizedPlausibleProviderProps> = ({
+  children,
+}) => {
+  if (!process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN) {
+    return children;
+  }
+
+  return (
+    <PlausibleProvider
+      customDomain={process.env.NEXT_PUBLIC_PLAUSIBLE_HOST}
+      trackOutboundLinks
+      domain={process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN}
+    >
+      {children}
+    </PlausibleProvider>
+  );
+};
+
 type ProvidersProps = {
   children: ReactNode;
 };
@@ -14,13 +36,7 @@ export const Providers: FC<ProvidersProps> = ({ children }) => (
     <SWRConfig
       value={{ fetcher: (url) => fetch(url).then((res) => res.json()) }}
     >
-      <PlausibleProvider
-        customDomain="https://insights.wsky.dev"
-        trackOutboundLinks
-        domain="digger.tools"
-      >
-        {children}
-      </PlausibleProvider>
+      <CustomizedPlausibleProvider>{children}</CustomizedPlausibleProvider>
     </SWRConfig>
   </ThemeProvider>
 );
