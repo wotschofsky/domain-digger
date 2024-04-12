ba53f8ed4135eb78c9010eb9c9ee978fdbd7f0b5
Felix Wotschofsky <felix@wotschofsky.com>
2024-04-12 18:27:31 +0200

    Add search form to 404 pages 🕵️‍♂️

--- app/lookup/not-found.tsx
+++ app/lookup/not-found.tsx
@@ -1,11 +1,14 @@
-import { type FC } from 'react';
+import type { FC } from 'react';
+
+import { SearchForm } from '../_components/search-form';
 
 const LookupNotFound: FC = () => (
-  <div className="mt-12 flex flex-col items-center gap-2">
-    <h2>404 - Not Found</h2>
-    <p className="mt-2 text-center text-sm text-muted-foreground">
-      Not a valid domain
-    </p>
+  <div className="container mt-12 flex flex-col items-center gap-2">
+    <h2 className="text-xl font-bold">Not a valid domain</h2>
+
+    <div className="mt-16 w-full max-w-2xl">
+      <SearchForm autofocus />
+    </div>
   </div>
 );
 
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,14 @@
-import { type FC } from 'react';
+import type { FC } from 'react';
+
+import { SearchForm } from './_components/search-form';
 
 const GlobalNotFound: FC = () => (
-  <div className="mt-12 flex flex-col items-center gap-2">
-    <h2>404 - Not Found</h2>
-    <p className="mt-2 text-center text-sm text-muted-foreground">
-      This page doesn&apos;t exist
-    </p>
+  <div className="container mt-12 flex flex-col items-center gap-2">
+    <h2 className="text-xl font-bold">Page not found</h2>
+
+    <div className="mt-16 w-full max-w-2xl">
+      <SearchForm autofocus />
+    </div>
   </div>
 );
 
