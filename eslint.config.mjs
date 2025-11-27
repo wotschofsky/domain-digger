7574777c3cff6cf4ccb8d63dd942d3995cb387ef
Felix Wotschofsky <felix@wotschofsky.com>
2025-11-27 23:43:15 +0100

    disable eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,17 @@ export default [
       'no-console': ['error', { allow: ['error', 'info', 'warn'] }],
       '@typescript-eslint/consistent-type-imports': 'error',
       'react-compiler/react-compiler': 'error',
+
+      // TODO: Temporary - convert these back to errors once fixed
+      // These rules are set to warn temporarily and should be removed soon
+      '@typescript-eslint/ban-ts-comment': 'warn',
+      '@typescript-eslint/consistent-type-imports': 'warn',
+      '@typescript-eslint/no-empty-object-type': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unsafe-function-type': 'warn',
+      'react-hooks/preserve-manual-memoization': 'warn',
+      'react-hooks/purity': 'warn',
+      'react-hooks/set-state-in-effect': 'warn',
     },
   },
 ];
