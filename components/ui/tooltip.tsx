329a19b3d9df6fcb0bf2f418ea05070330eb67db
Felix Wotschofsky <felix@wotschofsky.com>
2025-12-19 17:55:26 +0100

    Fix popover colors not matching 🎨💥

--- components/ui/tooltip.tsx
+++ components/ui/tooltip.tsx
@@ -19,7 +19,7 @@ const TooltipContent = React.forwardRef<
     ref={ref}
     sideOffset={sideOffset}
     className={cn(
-      'bg-popover text-popover-foreground animate-in fade-in-50 data-[side=bottom]:slide-in-from-top-1 data-[side=left]:slide-in-from-right-1 data-[side=right]:slide-in-from-left-1 data-[side=top]:slide-in-from-bottom-1 z-50 overflow-hidden rounded-md border px-3 py-1.5 text-sm shadow-md',
+      'bg-white text-zinc-950 animate-in fade-in-50 data-[side=bottom]:slide-in-from-top-1 data-[side=left]:slide-in-from-right-1 data-[side=right]:slide-in-from-left-1 data-[side=top]:slide-in-from-bottom-1 z-50 overflow-hidden rounded-md border border-zinc-200 px-3 py-1.5 text-sm shadow-md dark:bg-zinc-900 dark:text-zinc-50 dark:border-zinc-800',
       className,
     )}
     {...props}
