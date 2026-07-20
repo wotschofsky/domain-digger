cf0eccf40322bf501a1dc4ed337e7c602e067e6b
Felix Wotschofsky <felix@wotschofsky.com>
2026-07-20 22:32:48 +0200

    Convert Next.js config to TS 🔌📜

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   reactStrictMode: false,
   turbopack: {
     rules: {
@@ -56,4 +57,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
